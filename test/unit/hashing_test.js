90d142e1b02e5a70e87fe70384ad95554ff41e2f
jwebber92 <joewebber.92@gmail.com>
2018-08-02 21:00:46 +0100

    Updating unit tests to use blake2 hashing

--- test/unit/hashing_test.js
+++ test/unit/hashing_test.js
@@ -1,26 +1,26 @@
 
 describe('StellarBase#hash', function() {
 
-  it("hashes a string properly, using SHA256", function() {
+  it("hashes a string properly, using BLAKE2", function() {
     let msg         = "hello world";
-    let expectedHex = "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
+    let expectedHex = "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610";
     let actualHex   = StellarBase.hash(msg).toString('hex');
 
     expect(actualHex).to.eql(expectedHex);
   });
 
 
-  it("hashes a buffer properly, using SHA256", function() {
+  it("hashes a buffer properly, using BLAKE2", function() {
     let msg         = Buffer.from("hello world", 'utf8');
-    let expectedHex = "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
+    let expectedHex = "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610";
     let actualHex   = StellarBase.hash(msg).toString('hex');
 
     expect(actualHex).to.eql(expectedHex);
   });
 
-  it("hashes an array of bytes properly, using SHA256", function() {
+  it("hashes an array of bytes properly, using BLAKE2", function() {
     let msg         = [ 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100 ];
-    let expectedHex = "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
+    let expectedHex = "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610";
     let actualHex   = StellarBase.hash(msg).toString('hex');
 
     expect(actualHex).to.eql(expectedHex);

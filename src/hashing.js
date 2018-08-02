47194a8f4a6b8121057abf2f6b549503c04db0c3
jwebber92 <joewebber.92@gmail.com>
2018-08-02 21:00:19 +0100

    Using blake2 module not blakejs

--- src/hashing.js
+++ src/hashing.js
@@ -1,13 +1,15 @@
 import {sha256} from "sha.js";
 
-import {blake2b} from "blakejs";
+import {createHash} from "blake2";
+
+// export function hash(data) {
+//   let hasher = new sha256();
+//   hasher.update(data, 'utf8');
+//   return hasher.digest();
+// }
 
 export function hash(data) {
-  let hasher = new sha256();
-  hasher.update(data, 'utf8');
-  return hasher.digest();
-}
-
-export function blake2(data) {
-  return blake2b.blake2b(data);
+  let hash = createHash('blake2b', {digestLength: 32});
+  hash.update(new Buffer(data));
+  return hash.digest("hex");
 }

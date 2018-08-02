57cff4f5037b10f3de7e649ab5b43cd759d48de4
jwebber92 <joewebber.92@gmail.com>
2018-08-02 12:08:46 +0100

    Created blake2b hashing function

--- src/hashing.js
+++ src/hashing.js
@@ -1,7 +1,13 @@
 import {sha256} from "sha.js";
 
+import {blake2b} from "blakejs";
+
 export function hash(data) {
   let hasher = new sha256();
   hasher.update(data, 'utf8');
   return hasher.digest();
+}
+
+export function blake2(data) {
+  return blake2b.blake2b(data);
 }
